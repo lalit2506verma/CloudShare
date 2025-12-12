87311712152617e1103b68675f5b35a428f3e0bc
Lalit Kumar Verma <lalit2506verma@gmail.com>
2025-12-12 18:55:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="text-4xl font-bold">
+      Whereas disregard and contempt for human rights have resulted
+    </div>
+  );
+}
+
+export default App
